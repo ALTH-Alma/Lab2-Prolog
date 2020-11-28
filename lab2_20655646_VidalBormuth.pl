8d30c46bbd3120daf6ffe6870cb7ae5f5fb47633
Alma Vidal <alma.vidal@usach.cl>
2020-11-28 16:26:13 -0400

    Avance funcion login

--- lab2_20655646_VidalBormuth.pl
+++ lab2_20655646_VidalBormuth.pl
@@ -60,7 +60,8 @@ getMontoRecompensa([O,MT],MT):- esRecompensa([O,MT]).
 %una lista de strings LR (lista de referencias del usuario) y una variable para asignar al usuario.
 %Salida: un Usuario (lista de elementos) si las entradas son validas y un false sino.
 
-usuario(NU,CU,RU,LR, USUARIO):-
+usuario([]).
+crearUsuario(NU,CU,RU,LR, USUARIO):-
 string(NU), string(CU), integer(RU), esListaString(LR),
 RU > -1,
 USUARIO= [NU,CU,RU,LR].
@@ -74,6 +75,7 @@ USUARIO= [NU,CU,RU,LR].
 %Entrada: una lista de elementos que sirve como postulante a un usuario.
 %Salida: un booleano, un trus si la lista es un usuario y un false sino.
 
+esUsuario([]).
 esUsuario([NU,CU,RU,LR]):-
 string(NU), string(CU), integer(RU), esListaString(LR),
 RU > -1 .
@@ -96,33 +98,44 @@ getRefeUser([NU,CU,RU,LR],LR):- esUsuario([NU,CU,RU,LR]).
 
 %_________________________________________
 
-%TDA usuarios: representan una lista de usuarios en el stack.
-%Representación: una listade usuarios [usuario1, usuario2, ......., usuarioN].
+%TDA lista de usuarios: representa una lista de usuarios en el stack.
+%Representación: una lista de usuarios [usuario1, usuario2, ......., usuarioN].
 
 %Capa pertenencia:
 %Entrada: una lista con elementos, sirve como postulante para una lista de usuarios.
-%Salida: un booleano, un trus si la lista corresponde a una lista de usuarios.
+%Salida: un booleano, un true si la lista corresponde a una lista de usuarios.
+
+esListaUserVacia([]).
 esListaUsuarios([]).
 esListaUsuarios([H|T]):- esUsuario(H),esListaUsuarios(T).
 
-%Capa constructor:
-%Entrada: una lista de usuarios o una nueva lista vacia, y un nuevo usuario.
+
+%Capa modificador:
+%Entrada: una lista de usuarios o una nueva lista vacia, el nuevo usuario y una variable para asignar la nueva lista de usuarios con el usuario incluido.
 %Salida: un nueva lista de usuarios.
+
 agregarUsuario([], [NU,CU,RU,LR], [[NU,CU,RU,LR]]):- esUsuario([NU,CU,RU,LR]). %caso base
-agregarUsuario([H|T], [NU,CU,RU,LR],  [H|NEWT]):- 
+agregarUsuario([H|T], [NU,CU,RU,LR], [H|NEWT]):- 
 esListaUsuarios([H|T]),
 agregarUsuario(T, [NU,CU,RU,LR], NEWT). 
 
 %Ejemplo: [["Maria", "Maria1999", 50, ["Racket","c#"]],["Ana","A1234", 70, ["java","python"]],["Juan","juan2000", 20, ["python","c++"]]].
 
 %Capa selector: entrega a un usuario en una lista de usuarios al idendificarlo por su nombre.
-
+%Entrada: una lista de usuarios, un string NOMBRE (nombre del usuario que se desea obtener) y una variable U para asignar al usuario.
+%Salida: un usuario.
 getUsuario([H|_], NOMBRE, U):- 
 getNameUser(H,NU), NU == NOMBRE, U= H.
 
 getUsuario([_|T], NOMBRE, U):-
 getUsuario(T, NOMBRE, U).
 
+%Función extra: verifica si existe un usuario en la lista de usuarios.
+existeUsuario([H|_], NOMBRE):- 
+getNameUser(H,NU), NU == NOMBRE, !.
+
+existeUsuario([_|T], NOMBRE):-
+existeUsuario(T, NOMBRE).
 
 
 %_________________________________________
@@ -135,12 +148,13 @@ getUsuario(T, NOMBRE, U).
 %Entrada: entero IDR (identificador respuesta), string AR (autor de la respuesta), fecha FP (fecha de publicación), string C (contenido), 
 %lista de strings LE (lista de etiquetas), string EA (estado de aceptacion), entero VF (votos a favor), entero VC (votos en contra), 
 %entero NR (entero de reportes) y una variable RESPUESTA para asignar la respuesta contruida.
-Salida: un Respuesta (lista de elementos) si las entradas son correctas y un false sino.
+%Salida: un Respuesta (lista de elementos) si las entradas son correctas y un false sino.
 
-respuesta(IDR, AR, FP, C, LE, EA, VF, VC, NR, RESPUESTA):-
+respuesta([]).
+crearRespuesta(IDR, AR, FP, C, LE, EA, VF, VC, NR, RESPUESTA):-
 integer(IDR), string(AR), esFecha(FP), string(C), esListaString(LE), string(EA), integer(VF), integer(VC), integer(NR), 
 IDR > -1, VF > -1, VC > -1, NR > -1,
-RESPUESTA=[IDR, AR, FP, C, LE, EA, VF, VC, NR, RESPUESTA].
+RESPUESTA= [IDR, AR, FP, C, LE, EA, VF, VC, NR].
 
 %Ejemplo R1= [0,"Pedro", [2,3,2020], "Aumenta la complejidad y puede generar resultados impredecibles", ["Variables globales", "Problemas"], "Aceptada",5,2,0].
 
@@ -148,6 +162,7 @@ RESPUESTA=[IDR, AR, FP, C, LE, EA, VF, VC, NR, RESPUESTA].
 %Entrada: una lista de elementos que sirve como postulante a ser una respuesta.
 %Salida: un booleano, un true si la lista es una respuesta y un false sino.
 
+esRespuesta([]).
 esRespuesta([IDR, AR, FP, C, LE, EA, VF, VC, NR]):-
 integer(IDR), string(AR), esFecha(FP), string(C), esListaString(LE), string(EA), integer(VF), integer(VC), integer(NR),
 IDR > -1, VF > -1, VC > -1, NR > -1 .
@@ -156,7 +171,7 @@ IDR > -1, VF > -1, VC > -1, NR > -1 .
 %Entrada: todas los predicados de la capa selector reciben una respuesta y una variable para guardar el dato buscado.
 
 %Salida: un entero, ID de la respuesta.
-getIDRes([IDR, AR, FP, C, LE, EA, VF, VC, NR],IDR):- esRespuesta([IDR, AR, FP, C, LE, EA, VF, VC, NR]).
+getIdRes([IDR, AR, FP, C, LE, EA, VF, VC, NR],IDR):- esRespuesta([IDR, AR, FP, C, LE, EA, VF, VC, NR]).
 
 %Salida: un string, nombre del autor de la respuesta.
 getAutorRes([IDR, AR, FP, C, LE, EA, VF, VC, NR],AR):- esRespuesta([IDR, AR, FP, C, LE, EA, VF, VC, NR]).
@@ -182,6 +197,41 @@ getVotosEnContraRes([IDR, AR, FP, C, LE, EA, VF, VC, NR],VC):- esRespuesta([IDR,
 %Salida: un entero, reportes de la respuesta.
 getReportesRes([IDR, AR, FP, C, LE, EA, VF, VC, NR],NR):- esRespuesta([IDR, AR, FP, C, LE, EA, VF, VC, NR]).
 
+%_________________________________________
+
+%TDA lista de respuestas: representa una lista de respuestas en una pregunta.
+%Representación: una lista de respuestas [respuesta1, respuesta2, ......., respuestaN].
+
+%Capa pertenencia:
+%Entrada: una lista con elementos, sirve como postulante para una lista de respuestas.
+%Salida: un booleano, un true si la lista corresponde a una lista de respuestas y un false sino.
+esListaRespuestasVacia([]).
+esListaRespuestas([]).
+esListaRespuestas([H|T]):- esRespuesta(H),esListaRespuestas(T).
+
+%Capa constructor:
+%Entrada: una lista de respuestas o una nueva lista vacia, la nueva respuesta y una variable para asignar la nueva lista de respuestas con la respuesta incluida.
+%Salida: una nueva lista de respuestas.
+
+agregarRespuesta([], [IDR, AR, FP, C, LE, EA, VF, VC, NR], [[IDR, AR, FP, C, LE, EA, VF, VC, NR]]):- 
+esRespuesta([IDR, AR, FP, C, LE, EA, VF, VC, NR]). %caso base
+
+agregarRespuesta([H|T], [IDR, AR, FP, C, LE, EA, VF, VC, NR], [H|NEWT]):- 
+esListaRespuestas([H|T]),
+agregarRespuesta(T, [IDR, AR, FP, C, LE, EA, VF, VC, NR], NEWT). 
+
+%Ejemplo: [[0,"Pedro", [2,3,2020], "Aumenta la complejidad y puede generar resultados impredecibles", ["Variables globales", "Problemas"], "Aceptada",5,2,0],[1, "Juan", [1, 3, 2020], "El problema de las variables globales es que crea dependencias ocultas. Cuando se trata de aplicaciones grandes, ni tú mismo sabes/recuerdas/tienes claro los objetos que tienes y sus relaciones.", ["Malas practicas","errores"], "", 2, 9, 1],[2, "Maria", [13, 11, 2020], "Usando Qt Style Sheet", [], "Rechazada", 6, 3, 0]].
+
+%Capa selector: entrega una respuesta en una lista de respuesta al idendificarla por su id.
+%Entrada: una lista de respuestas, un entero IDR (identificador de la respuesta que se desea obtener) y una variable R para asignar la respuesta.
+%Salida: una respuestas.
+
+getRespuesta([H|_], IDR, R):- 
+getIdRes(H,ID), ID == IDR, R= H.
+
+getRespuesta([_|T], IDR, R):-
+getRespuesta(T, IDR, R).
+
 
 %_________________________________________
 
@@ -197,19 +247,21 @@ getReportesRes([IDR, AR, FP, C, LE, EA, VF, VC, NR],NR):- esRespuesta([IDR, AR, 
 %(repuestas de la pregunta), una variable PREGUNTA para asignar la pregunta construida. 
 %Salida: una Pregunta (lista de elementos) si las entradas son correctas y un false sino. 
 
-pregunta(IDP,AP,FP,C,LE,EP,NV,VF,VC,REC,NR,RESPUESTAS,PREGUNTA):-
-integer(IDP),string(AP),esFecha(FP),string(C),esListaString(LE),string(EP),integer(NV),integer(VF),integer(VC),esRecompensa(REC),integer(NR),
+pregunta([]).
+crearPregunta(IDP,AP,FP,C,LE,EP,NV,VF,VC,REC,NR,RESPUESTAS,PREGUNTA):-
+integer(IDP),string(AP),esFecha(FP),string(C),esListaString(LE),string(EP),integer(NV),integer(VF),integer(VC),esRecompensa(REC),integer(NR),esListaRespuestas(RESPUESTAS),
 IDP > -1, NV > -1, VF > -1, VC > -1, NR > -1,
 PREGUNTA= [IDP,AP,FP,C,LE,EP,NV,VF,VC,REC,NR,RESPUESTAS].
 
-%Ejemplo R1= [0, "Maria", [29, 2, 2020], "¿Por qué es considerado una mala práctica utilizar variables globales?,¿Realmente son perjudiciales?", ["Malas practicas","variables globales"], "Abierta", 10, 3, 2, ["Maria", 10], 1, [0,"Pedro", [2,3,2020], "Aumenta la complejidad y puede generar resultados impredecibles", ["Variables globales", "Problemas"], "Aceptada",5,2,0]].
+%Ejemplo R1= [0, "Maria", [29, 2, 2020], "¿Por qué es considerado una mala práctica utilizar variables globales?,¿Realmente son perjudiciales?", ["Malas practicas","variables globales"], "Abierta", 10, 3, 2, ["Maria", 10], 1, [[0,"Pedro", [2,3,2020], "Aumenta la complejidad y puede generar resultados impredecibles", ["Variables globales", "Problemas"], "Aceptada",5,2,0]]].
 
 %Capa pertenencia:
 %Entrada: una lista de elementos que sirve como postulante a ser una pregunta.
 %Salida: un booleano, un true si la lista es una pregunta y un false sino.
 
+esPregunta([]).
 esPregunta([IDP,AP,FP,C,LE,EP,NV,VF,VC,REC,NR,RESPUESTAS]):-
-integer(IDP),string(AP),esFecha(FP),string(C),esListaString(LE),string(EP),integer(NV),integer(VF),integer(VC),esRecompensa(REC),integer(NR),
+integer(IDP),string(AP),esFecha(FP),string(C),esListaString(LE),string(EP),integer(NV),integer(VF),integer(VC),esRecompensa(REC),integer(NR),esListaRespuestas(RESPUESTAS),
 IDP > -1, NV > -1, VF > -1, VC > -1, NR > -1 .
 
 %Capa selector:
@@ -252,3 +304,128 @@ getReportesPreg([IDP,AP,FP,C,LE,EP,NV,VF,VC,REC,NR,RESPUESTAS],NR):- esPregunta(
 getRespuestasPreg([IDP,AP,FP,C,LE,EP,NV,VF,VC,REC,NR,RESPUESTAS],RESPUESTAS):- esPregunta([IDP,AP,FP,C,LE,EP,NV,VF,VC,REC,NR,RESPUESTAS]).
 
 
+%_________________________________________
+
+%TDA lista de preguntas: representa una lista de preguntas en el stack.
+%Representación: una lista de preguntas [pregunta1, pregunta2, ......., preguntaN].
+
+%Capa pertenencia:
+%Entrada: una lista con elementos, sirve como postulante para una lista de preguntas.
+%Salida: un booleano, un true si la lista corresponde a una lista de preguntas y un false sino.
+esListaPreguntasVacia([]).
+esListaPreguntas([]).
+esListaPreguntas([H|T]):- esPregunta(H),esListaPreguntas(T).
+
+%Capa constructor:
+%Entrada: una lista de respuestas o una nueva lista vacia, la nueva respuesta y una variable para asignar la nueva lista de respuestas con la respuesta incluida.
+%Salida: una nueva lista de respuestas.
+
+agregarPregunta([], [IDP,AP,FP,C,LE,EP,NV,VF,VC,REC,NR,RESPUESTAS], [[IDP,AP,FP,C,LE,EP,NV,VF,VC,REC,NR,RESPUESTAS]]):-
+esPregunta([IDP,AP,FP,C,LE,EP,NV,VF,VC,REC,NR,RESPUESTAS]). 
+
+agregarPregunta([H|T], [IDP,AP,FP,C,LE,EP,NV,VF,VC,REC,NR,RESPUESTAS],  [H|NEWT]):- 
+esListaPreguntas([H|T]),
+agregarPregunta(T, [IDP,AP,FP,C,LE,EP,NV,VF,VC,REC,NR,RESPUESTAS], NEWT). 
+
+%Capa selector: entrega a una pregunta en una lista de preguntas al idendificarla por su id.
+%Entrada: una lista de preguntas, un entero IDP (identificador de la pregunta que se desea obtener) y una variable P para asignar la pregunta.
+%Salida: un pregunta.
+
+getPregunta([H|_], IDP, P):- 
+getIdPreg(H,ID), ID == IDP, P= H.
+
+getPregunta([_|T], IDP, P):-
+getPregunta(T, IDP, P).
+
+
+ %_________________________________________
+
+%TDA stack: representa a Stack Overflow.
+%Representación: stack [usuario Sesión activa, lista de usuarios Registrados, lista de preguntas Preguntas stack, entero Correlativo pregunta, entero Correlativo respuestas].
+
+%Capa constructor:
+%Entrada: un usuario UA (usuario activo en el stack), una lista de usuarios LU (usuarios registrados en el stack), una lista de preguntas LP (preguntas del stack),
+% un entero CP (correlativo para llevar la cuenta de la cantidad de preguntas en el stack), un entero CR (correlativo para llevar la cuenta de la cantidad de respuestas en el stack).
+%Salida: un stack.
+
+%stack([[],[],[],0,0]).
+crearStack(UA, LU, LP, CP, CR, STACK):-
+esUsuario(UA),esListaUsuarios(LU),esListaPreguntas(LP),integer(CP),integer(CR),
+CP > -1 , CR > -1,
+STACK= [UA, LU, LP, CP, CR].
+
+%Capa pertenencia:
+%Entrada: un lista de elementos que sirven como postulante a ser un stack.
+%salida: un boolenao, un true si la lista es un stack y un false sino.
+
+esStack([[],[],[],0,0]).
+esStack([UA, LU, LP, CP, CR]):-
+esUsuario(UA),esListaUsuarios(LU),esListaPreguntas(LP),integer(CP),integer(CR),
+CP > -1 , CR > -1.
+
+%Capa selector:
+%Entrada: todos los predicados de la capa selector reciben un stack.
+
+%Salida: un usuario, usuario activo en el stack.
+getActivo([UA, LU, LP, CP, CR],UA):- esStack([UA, LU, LP, CP, CR]).
+
+%Salida: una lista de usuarios, registrados en el stack.
+getListaUsuarios([UA, LU, LP, CP, CR],LU):- esStack([UA, LU, LP, CP, CR]).
+
+%Salida: una lista de preguntas, preguntas del stack.
+getListaPreguntas([UA, LU, LP, CP, CR],LP):- esStack([UA, LU, LP, CP, CR]).
+
+%Salida: un entero, correlativo de preguntas en el stack.
+getCorrelativoPreg([UA, LU, LP, CP, CR],CP):- esStack([UA, LU, LP, CP, CR]).
+
+%Salida: un entero, correlativo de respuestas en el stack.
+getCorrelativoRes([UA, LU, LP, CP, CR],CR):- esStack([UA, LU, LP, CP, CR]).
+
+%_________________________________________
+%Desarrollo requerimiento 2: Hechos.
+
+usuario(["Maria", "Maria1999", 50, ["Racket","c#"]]).
+usuario(["Ana","A1234", 70, ["java","python"]]).
+usuario(["Juan","juan2000", 20, ["python","c++"]]).
+usuario(["Pedro", "P340", 90,  ["python","c++"]]).
+
+stack([[],[["Maria", "Maria1999", 50, ["Racket","c#"]],["Ana","A1234", 70, ["java","python"]],["Juan","juan2000", 20, ["python","c++"]]], 
+			[[0, "Maria", [29, 2, 2020], "¿Por que es considerado una mala practica utilizar variables globales?,¿Realmente son perjudiciales?", 
+			["Malas practicas","variables globales"], "Abierta", 10, 3, 2, ["Maria", 10], 1, 
+ 			[[0,"Pedro", [2,3,2020], "Aumenta la complejidad y puede generar resultados impredecibles", ["Variables globales", "Problemas"], "Aceptada",5,2,0]]],
+			[1, "Ana", [29, 10, 2020], "¿Como poner una imagen de fondo en? Me gustaria saber ¿Como pongo una imagen de fondo a la ventana creada con PyQT5? Muchos me dicen que use Designer, pero estoy evitando usarlo. ¿Conocen alguna manera?", ["python","interfaz-gráfica","imagen"], "Abierta", 20, 5, 2,["",0], 0,
+			[[0,"Pedro", [2,3,2020], "Aumenta la complejidad y puede generar resultados impredecibles", ["Variables globales", "Problemas"], "Aceptada",5,2,0],
+			[1, "Juan", [1, 3, 2020], "El problema de las variables globales es que crea dependencias ocultas. Cuando se trata de aplicaciones grandes, ni tu mismo sabes/recuerdas/tienes claro los objetos que tienes y sus relaciones.", ["Malas practicas","errores"], "", 2, 9, 1],
+ 			[2, "Maria", [13, 11, 2020], "Usando Qt Style Sheet", [], "Rechazada", 6, 3, 0]]] ], 
+ 			10, 12]).
+
+stack([[],[["Maria", "Maria1999", 50, ["Racket","c#"]],["Ana","A1234", 70, ["java","python"]],["Juan","juan2000", 20, ["python","c++"]],["an","H123",0,[]]], 
+			[[0, "Maria", [29, 2, 2020], "¿Por que es considerado una mala practica utilizar variables globales?,¿Realmente son perjudiciales?", 
+			["Malas practicas","variables globales"], "Abierta", 10, 3, 2, ["Maria", 10], 1, 
+ 			[[0,"Pedro", [2,3,2020], "Aumenta la complejidad y puede generar resultados impredecibles", ["Variables globales", "Problemas"], "Aceptada",5,2,0]]],
+			[1, "Ana", [29, 10, 2020], "¿Como poner una imagen de fondo en? Me gustaria saber ¿Como pongo una imagen de fondo a la ventana creada con PyQT5? Muchos me dicen que use Designer, pero estoy evitando usarlo. ¿Conocen alguna manera?", ["python","interfaz-gráfica","imagen"], "Abierta", 20, 5, 2,["",0], 0,
+			[[0,"Pedro", [2,3,2020], "Aumenta la complejidad y puede generar resultados impredecibles", ["Variables globales", "Problemas"], "Aceptada",5,2,0],
+			[1, "Juan", [1, 3, 2020], "El problema de las variables globales es que crea dependencias ocultas. Cuando se trata de aplicaciones grandes, ni tu mismo sabes/recuerdas/tienes claro los objetos que tienes y sus relaciones.", ["Malas practicas","errores"], "", 2, 9, 1],
+ 			[2, "Maria", [13, 11, 2020], "Usando Qt Style Sheet", [], "Rechazada", 6, 3, 0]]] ], 
+ 			10, 12]).
+
+
+
+
+
+
+%Desarrollo función register:
+
+register(SK,NEWUSERNAME, PASS, SKS):-
+esStack(SK),string(NEWUSERNAME),string(PASS),string(PASS),
+getListaUsuarios(SK,LU),(not(existeUsuario(LU,NEWUSERNAME))),
+crearUsuario(NEWUSERNAME,PASS,0,[],NEWUSER),agregarUsuario(LU,NEWUSER,NLU),
+getActivo(SK,UA),
+getListaPreguntas(SK,LP),
+getCorrelativoPreg(SK,CP),
+getCorrelativoRes(SK,CR),
+crearStack(UA, NLU, LP, CP, CR, NSK),
+(not(SKS == NSK)), !, var(SKS), SKS= NKS.
+
+
+esIgual(A,B):- (A == B), !, var(B), B=A. 
